e08ff052068420f99eef4f26ab466732eef733a0
bening14 <agus_salim_83@yahoo.co.id>
2023-06-18 11:08:08 +0700

    update

--- application/views/auth/auth-success.php
+++ application/views/auth/auth-success.php
@@ -49,7 +49,7 @@
                                     <img src="<?= base_url('assets/default/assets/images/' . $logo_light) ?>" alt="" height="28">
                                 </a>
                             </div>
-                            <p class="mt-3 fs-15 fw-medium">Sistem Informasi Sosiometri - E-Sosiometri</p>
+                            <p class="mt-3 fs-15 fw-medium">SI MITRI - Solusi Sosiometri</p>
                         </div>
                     </div>
                 </div>
